a855f4588c1eadadb7810b51741a9d4b43f9419b
Roderick Hsiao <roderickhsiao@gmail.com>
2025-10-22 23:17:44 -0700

    chore: Bump Nextjs 16

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,9 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const nextConfig: NextConfig = {
+  reactCompiler: true,
   htmlLimitedBots: /Mediapartners-Google|Slurp|DuckDuckBot|baiduspider|yandex|sogou|bitlybot|tumblr|vkShare|quora link preview|redditbot|ia_archiver|Bingbot|BingPreview|applebot|facebookexternalhit|facebookcatalog|Twitterbot|LinkedInBot|Slackbot|Discordbot|WhatsApp|SkypeUriPreview/i,
   experimental: {
-    reactCompiler: true,
     viewTransition: true,
     inlineCss: true,
     optimizeCss: true,
